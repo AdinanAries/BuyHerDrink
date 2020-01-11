333c23dce0e3aa32814d7814cffa688bf6fdaa8c
Mohammed Adinan <adinanaries@outlook.com>
2020-01-11 10:46:40 -0500

    working on the Menu Animation

--- build/web/Scripts/MainScript.js
+++ build/web/Scripts/MainScript.js
@@ -2,12 +2,33 @@ document.querySelector(".MainMenu").style.display = "none";
 
 function ToggleMenuDisplay() {
     var MenuDisplay = document.querySelector(".MainMenu").style.display;
+    document.querySelector(".MainMenu").style.cssText += 'position: absolute; display: block;';
     
     if(MenuDisplay === "none"){
+        $(".MainMenu").show("slide", { direction: "left" }, 100);
         
-        $(".MainMenu").fadeIn("fast");
+        $("#secondMenuIconBar").fadeOut("fast");
+        
+        $("#firstMenuIconBar").css({'transition' : '0.5s'});
+        document.getElementById("firstMenuIconBar").style.cssText += '';
+        $("#firstMenuIconBar").css({'transform' : 'translateY(12px)'});
+        $("#firstMenuIconBar").css({'transform' : 'rotate('+ 45 +'deg)'}, 100);
+        
+        $("#thirdMenuIconBar").css({'transition' : '0.5s'});
+        document.getElementById("thirdMenuIconBar").style.cssText += 'top: 10px; dsplay: inline-block;';
+        $("#thirdMenuIconBar").css({'transform' : 'translateY(-12px)'});
+        $("#thirdMenuIconBar").css({'transform' : 'rotate('+ -45 +'deg)'}, 100);
+        
+        //$(".MainMenu").fadeIn("fast");
     }else{
-        $(".MainMenu").fadeOut("fast");
+        $(".MainMenu").hide("slide", { direction: "left" }, 100);
+        $("#secondMenuIconBar").fadeIn("fast");
+        $("#firstMenuIconBar").css({'transition' : '0.5s'});
+        $("#firstMenuIconBar").css({'transform' : 'rotate('+ 0 +'deg)'}, 100);
+        $("#thirdMenuIconBar").css({'transition' : '0.5s'});
+        $("#thirdMenuIconBar").css({'transform' : 'rotate('+ 0 +'deg)'}, 100);
+        
+        //$(".MainMenu").fadeOut("fast");
     }
 }
 
